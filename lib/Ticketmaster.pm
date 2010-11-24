2446172268b96174a17166535ab711df80a351ea
PAN YU <yu.pan1005@gmail.com>
2010-11-24 06:20:56 +0000

    Import of PANYU/Ticketmaster-0.01 from CPAN.
    gitpan-cpan-distribution: Ticketmaster
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PANYU/Ticketmaster-0.01.tar.gz
    gitpan-cpan-author:       PANYU
    gitpan-cpan-maturity:     released

---
+++ lib/Ticketmaster.pm
@@ -0,0 +1,74 @@
+package Ticketmaster;
+
+use 5.008008;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Ticketmaster ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+This module is to calculate the change in coins
+
+=head1 SYNOPSIS
+
+  use Ticketmaster;
+
+=head1 DESCRIPTION
+
+The module has been almost done. Before distribute it on cpan, I would like to test it thoroughly.
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Pan Yu, E<lt>xiaocong[AT]vip.163.com<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by panyu
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
+
